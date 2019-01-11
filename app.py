d3d998f81cd21c02d0ac7ad00e9b7e8728cacc5f
Riley Flynn <riley@rileyflynn.me>
2019-01-11 17:00:32 -0330

    Slightly cleaned up python code

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from urllib.parse import urlparse, parse_qs
+
 import requests
 from flask import Flask, jsonify
-from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def get_busses():
@@ -11,9 +13,11 @@ def get_busses():
     soup = BeautifulSoup(response.text, 'html.parser')
     a_tags = soup.find_all('a', href=True)
     data = {}
+
     for tag in a_tags:
         url = tag['href']
         parsedUrl = urlparse(url)
+
         if parsedUrl.path == '/bus_locate.asp':
             queryParams = parse_qs(parsedUrl.query)
             data[queryParams['route'][0]] = {
@@ -21,4 +25,5 @@ def get_busses():
                 'lon': queryParams['lon'][0],
                 'position_time': queryParams['position_time'][0],
             }
+
     return jsonify(data)
